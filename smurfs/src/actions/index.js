67c0facb44ee27dae73a599f0c7bb9d6a1f96b98
Carla Marvin <carlamarvin78@gmail.com>
2019-08-18 15:30:09 -0400

    Fixed reducers

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export const GET_SMURF = 'GET_SMURF'
+export const ADD_SMURF = 'ADD_SMURF'
 
 export const getSmurfs = () => dispatch => {
     console.log("Getting smurfs");
@@ -14,6 +15,6 @@ export const addSmurf = (smurf) => dispatch => {
     console.log("Adding smurf: ", smurf);
     axios
         .post('http://localhost:3333/smurfs', smurf)
-        .then(response => dispatch({ type: GET_SMURF, payload: response.data }))
+        .then(response => dispatch({ type: ADD_SMURF, payload: response.data }))
         .catch(error => console.log(error));
 }
